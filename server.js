4035ffd02f11dba727f68ee0f2cc2a0b4b32e0a9
youssifessam2323 <yousfessaam2323@gmail.com>
2021-10-03 01:44:40 +0200

    finishing the task

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-// Setup empty JS object to act as endpoint for all routes
-let projectData = {
-    name: "youssif",
-    age: "21"
-};
+let projectData = {};
+
+
 const PORT = 8000;
-// Require Express to run server and routes
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// Start up an instance of app
 const app = express();
+
+
+/* Including my weather api client js file */
+const weatherApi = require('./weather');
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
 app.use(cors());
-// Initialize the main project folder
+
 app.use(express.static('website'));
 
 
-// Setup Server
+
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 })
@@ -33,4 +34,9 @@ app.get('/api/data', (req,res) => {
 
 app.post('/api/data', (req,res) => { 
     projectData = req.body;
+    res.send({message: "data added", status: 200});
 });
+
+app.get("/api/weather", async (req,res) => {
+    res.send(await weatherApi.requestWeatherByZipcode(req.query.zib));
+})
--- website/app.js
+++ website/app.js
@@ -1,12 +1,6 @@
-// const { json } = require("stream/consumers");
 
-/* Global Variables */
-const apiKey = process.env.API_KEY;
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather";
+const serverUrl = "http://localhost:8000"
 
-
-/*Util variables*/
-const queryStringChar = '?';
 
 /*dom elements */
 const tempDiv = document.querySelector("#temp");
@@ -14,16 +8,66 @@ const contentDiv = document.querySelector("#content");
 const dateDiv = document.querySelector("#date");
 const zipInput = document.querySelector("#zip");
 const submitBtn = document.querySelector("#generate");
-// Create a new date instance dynamically with JS
+const feelingInput = document.querySelector("#feelings");
+const feelingError = document.querySelector("#feeling-error");
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
 
 
 submitBtn.addEventListener("click", async e => {
-    const zipCode = zipInput.value;
-    await fetch(`${apiUrl}?zip=${zipCode}&appid=${apiKey}`)
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(e => console.log(e))
-});
 
+    //handling of the feel input is empty
+    handleTheEmptyFieldCase();
+
+    let userData = {
+        feeling : feelingInput.value,
+        zipcode : zipInput.value,
+    };
+
+    await fetch(`${serverUrl}/api/weather?zib=${userData.zipcode}`)
+            .then(res => res.json())
+            .then(async data => {
+
+                let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+                
+                if(data.cod =="404"){
+                   asyncAlertToUser(data.message);
+                }
+                userData = {...userData,
+                     temp:data.main.temp,
+                     date:newDate
+                     }
+                
+                updateTheUI(userData);
+
+
+                await fetch(`${serverUrl}/api/data`,{
+                    method:'POST',
+                    body: userData
+                }).then(res => res.text())
+                .then(d => console.log(d))
+
+            }).catch(e =>  console.log(e));
+        })
+
+
+const updateTheUI = ({feeling,temp,date}) => { 
+    tempDiv.textContent = temp;
+    contentDiv.textContent = feeling;
+    dateDiv.textContent = date
+}
+
+
+const asyncAlertToUser = async message => {
+    alert(message);
+}
+
+const handleTheEmptyFieldCase = () => {
+    if(feelingInput.value === ""){
+        feelingError.removeAttribute("hidden")
+        return;        
+    }else { 
+        feelingError.setAttribute("hidden","true");
+
+    }
+}
